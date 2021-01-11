9c17ae4b92f834d1efac9cea0d0b834531d9ec1d
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2021-01-10 20:59:22 -0600

    Make mods public

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 
-mod cipher;
+pub mod cipher;
 pub mod config;
 pub mod context;
 mod key_derivation;
 pub mod option;
-mod protection_profile;
+pub mod protection_profile;
 pub mod session;
 pub mod stream;
